ef315a38ae80038c0266373b8bcdf51a67c6e66f
Andrea <andrea.gavazzi@me.com>
2020-06-09 22:14:55 +0200

    mods

--- bin/example.py
+++ bin/example.py
@@ -26,6 +26,20 @@
 
 
 
+std_msgs/Header header
+  uint32 seq
+  time stamp
+  string frame_id
+string[] joint_names
+trajectory_msgs/JointTrajectoryPoint[] points
+  float64[] positions
+  float64[] velocities
+  float64[] accelerations
+  duration time_from_start
+
+
+
+
 import time
 import rospy
 from sensor_msgs.msg import JointState
--- bin/test.py
+++ bin/test.py
@@ -1,43 +1,41 @@
 #!/usr/bin/env python
 
 import rospy
-from trajectory_msgs.msg import JointTrajectory
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from dynamixel_workbench_msgs.msg import DynamixelStateList
 
 rospy.init_node('head_controller')
 rospy.loginfo('Tracker node started.')
 
 # crea oggetto bridge e il publisher per le coordinate
-head_pub = rospy.Publisher('joints', JointTrajectory, queue_size=1)
+head_pub = rospy.Publisher('joint_trajectory', JointTrajectory, queue_size=1)
+
 
 # funzione callback
 def callback(data):
-    try:
-    
-    blablabla
-    
-    joints = JointTrajectory()
-    
-    blablabla
-    
-    joints.name = ['pan', 'tilt']
-    joints.positions = [1, 0.5]
-    
-    head_pub.publish(joints)    
-    
-    except CvBridgeError, e:
-    print e
 
+    joints = JointTrajectory()
+    jp = JointTrajectoryPoint()
+    jp.positions.append(0)
+
+    joints.header.stamp = rospy.Time.now()
+    joints.header.frame_id = '/base_link'
+    joints.joint_names = ['pan']
+    joints.points.append(jp)
+
+
+    head_pub.publish(joints)
+    
 
 def main():
     # Sottoscrive lo stato dei dyna e pubblica il movimento
-    camera_sub = rospy.Subscriber("/dynamixel_State", dyna_state, callback)
+    camera_sub = rospy.Subscriber("/dynamixel_state", DynamixelStateList, callback)
 
     try:
         rospy.spin()
     except KeyboardInterrupt:
         # TODO: migliorare uscita dal programma
         print 'Process shutting down'
-        cv2.destroyAllWindows()
 
 
 # Main routine
